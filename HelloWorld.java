c1885ea93b2b95cb4815f6c3c8681f950cf7ecb7
R-raku-aforce <k.raku@a-force.co.jp>
2024-03-20 18:53:05 +0900

    delete SQL

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class HelloWorld {
+  
+	public static void main(String[] args){
+
+		ArrayList<String> fruits = new ArrayList<>();
+
+		friuts.add("apple");
+		friuts.add("orange");
+
+		System.out.println(fruits.get(0));
+
+		fruits.remove(0);
+		System.out.println(fruits.get(0));
+		
+
+  	}
+}
